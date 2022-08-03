fbb719acf2acd69f71c0a703d2882117647deadc
saurabhgulab <41892363+saurabhgulab@users.noreply.github.com>
2022-08-03 14:39:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ holdElement.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
